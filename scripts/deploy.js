1e20807d2e2d6e05e8fe895156dca352550625b5
ShubhamChndrvnshi <shubham.chndrvnshi@gmail.com>
2022-07-03 23:57:27 +0530

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,24 +6,34 @@
 const hre = require("hardhat");
 
 async function main() {
-  const MokeToken = await hre.ethers.getContractFactory("MokeToken");
+  const accounts = await ethers.getSigners();
 
-  console.log("Deploying X MokeToken...");
-  const xToken = await MokeToken.deploy("X Token", "XT");
-  await xToken.deployed();
-  console.log("X Token address:", xToken.address);
+  // Dpeloying referal cobntract
+  console.log("\nDeploying Referral");
+  console.log("Procuring artifacts");
+  const Referral = await hre.ethers.getContractFactory("Referral");
+  console.log("Sending transaction");
+  const referral = await Referral.deploy([5, 3, 2], "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", accounts[0].address);
+  console.log("Transaction sent");
+  console.log("Waiting for deployment");
+  await referral.deployed();
+  console.log("Waiting for block confirmation");
+  await referral.deployTransaction.wait();
+  console.log("Transaction confirmed");
 
-  console.log("Deploying USDT MokeToken...");
-  const usdt = await MokeToken.deploy("USD Token", "USDT");
-  await usdt.deployed();
-  console.log("USD Token address:", usdt.address);
+  // Deploying pool factory
+  console.log("\nDeploying Referral");
+  console.log("Procuring artifacts");
+  const PoolFactory = await hre.ethers.getContractFactory("PoolFactory");
+  console.log("Sending transaction");
+  const poolFactory = await PoolFactory.deploy("0xaE204EE82E60829A5850FE291C10bF657AF1CF02", accounts[0].address, referral.address);
+  console.log("Transaction sent");
+  console.log("Waiting for deployment");
+  await poolFactory.deployed();
+  console.log("Waiting for block confirmation");
+  await poolFactory.deployTransaction.wait();
+  console.log("Transaction confirmed");
 
-  //   const PoolFactory = await hre.ethers.getContractFactory("PoolFactory");
-  //   const poolFactory = await PoolFactory.deploy();
-
-  //   await poolFactory.deployed();
-
-  //   console.log("Greeter deployed to:", poolFactory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -32,3 +42,5 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+exports.deploy = main;
